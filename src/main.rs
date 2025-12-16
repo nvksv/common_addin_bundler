fadc81d921b93d266431eca8c536e54aaec432a0
Sergey V. Kokorin <nvksv@users.noreply.github.com>
2025-12-16 17:30:23 +0700

    .

--- src/main.rs
+++ src/main.rs
@@ -21,17 +21,22 @@ struct Cli {
 }
 
 struct BuildTarget {
+    cmd: &'static str,
     triple: &'static str,
     arch: &'static str,
     os: &'static str,
     archos: &'static str,
+    ext: &'static str,
 }
 
 const PKG_NAME: &'static str = "common_addin";
+const PKG_NAME_LINUX: &'static str = "libcommon_addin";
 
-const BUILD_TARGETS: [BuildTarget;2] = [
-    BuildTarget { triple: "i686-pc-windows-msvc",   arch: "i386",   os: "Windows", archos: "win32" },
-    BuildTarget { triple: "x86_64-pc-windows-msvc", arch: "x86_64", os: "Windows", archos: "win64" },
+const BUILD_TARGETS: [BuildTarget;4] = [
+    BuildTarget { cmd: "cargo", triple: "i686-pc-windows-msvc",     arch: "i386",   os: "Windows", archos: "win32", ext: "dll" },
+    BuildTarget { cmd: "cargo", triple: "x86_64-pc-windows-msvc",   arch: "x86_64", os: "Windows", archos: "win64", ext: "dll" },
+    BuildTarget { cmd: "cross", triple: "i686-unknown-linux-gnu",   arch: "i386",   os: "Linux", archos: "linux32", ext: "so" },
+    BuildTarget { cmd: "cross", triple: "x86_64-unknown-linux-gnu", arch: "x86_64", os: "Linux", archos: "linux64", ext: "so" },
 ];
 
 fn main() {
@@ -66,7 +71,7 @@ fn main() {
         .unix_permissions(0o755);
 
     for build_target in BUILD_TARGETS {
-        let mut cmd = Command::new("cargo");
+        let mut cmd = Command::new(build_target.cmd);
         cmd.arg("build");
         cmd.arg("--manifest-path");
         cmd.arg(&addin_manifest_path);
@@ -84,17 +89,21 @@ fn main() {
             .unwrap();
         assert!( status.status.success() );
 
+        let pkg_name = if build_target.os == "Linux" { PKG_NAME_LINUX } else { PKG_NAME };
+
         let original_dll_name = String::new() + 
-            PKG_NAME +
-            ".dll";
+            pkg_name +
+            "." + 
+            build_target.ext;
 
         let dll_with_timestamp_name = String::new() + 
-            PKG_NAME +
+            pkg_name +
             "." +
             build_target.archos +
             "." +
             &timestamp_str +
-            ".dll";
+            "." + 
+            build_target.ext;
         
         let original_dll_path = PathBuf::from(&cli.addin)
             .join("target")
